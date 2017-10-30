e62f36a1da79fdf97a4d151a67e3ed760fbb89c6
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-29 21:11:43 -0400

    Don't hardcode times

--- survey_user_finder.py
+++ survey_user_finder.py
@@ -1,28 +1,40 @@
 """
 selects users from subreddit using an unique set and returns [n] random ones
 """
-import random
-from typing import Set, List
+from typing import Set, List, Tuple
 
 import praw
 
+
 class Finder(object):
     """finds users"""
-    def __init__(self: Finder, reddit: praw.Reddit, subreddit: str, num: int = 100) -> None:
+
+    def __init__(self: Finder, reddit: praw.Reddit, subreddit: str, sample: int = 100,
+                 days_back: int = 30) -> None:
         self.reddit: praw.Reddit = reddit
         self.subreddit: praw.models.Subreddit = self.reddit.subreddit(subreddit)
-        self.num = num
+        self.sample = sample
+        self.days_back = days_back
 
     def find_users(self: Finder) -> Set[praw.models.Redditor]:
         """finds users"""
         redditors: Set[praw.models.Redditor] = set()
-        for submission in self.subreddit.submissions(1506816000, 1509408000):
+        times: Tuple[int, int] = self.get_times()
+        for submission in self.subreddit.submissions(times[0], times[1]):
             submission.comments.replace_more(limit=0)
             for comment in submission.comments.list():
                 redditors.add(comment.author)
 
         return redditors
 
+    def get_times(self: Finder) -> Tuple[int, int]:
+        """returns timestamps"""
+        import datetime
+        now: datetime.datetime = datetime.datetime.now(datetime.timezone.utc)
+        month_ago: datetime.datetime = now + datetime.timedelta(-self.days_back)
+        return (int(now.timestamp()), int(month_ago.timestamp()))
+
     def choose_users(self: Finder, reds: Set[praw.models.Redditor]) -> List[praw.models.Redditor]:
         """returns [n] amount of users"""
-        return random.choices(list(reds), k=self.num)
+        import random
+        return random.choices(list(reds), k=self.sample)
